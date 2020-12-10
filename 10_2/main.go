b956dfd295939d4f5c60b76b489b8b8887d99747
kkevinchou <>
2020-12-10 00:20:56 -0800

    cleanup

--- 10_2/main.go
+++ 10_2/main.go
@@ -23,29 +23,29 @@ func main() {
 
 	nums = append(nums, max+3, 0)
 	sort.Ints(nums)
-
 	fmt.Println(calc(nums))
 }
 
 func calc(nums []int) int {
-	countsFromPosition := map[int]int{}
-	countsFromPosition[len(nums)-1] = 1
-	countsFromPosition[len(nums)-2] = 1
-	countsFromPosition[len(nums)-3] = 1
-
-	for i := len(nums) - 4; i >= 0; i-- {
-		countsFromPosition[i] = subCalc(nums, countsFromPosition, i)
+	countAtPosition := map[int]int{
+		len(nums) - 1: 1,
+		len(nums) - 2: 1,
+		len(nums) - 3: 1,
 	}
 
-	fmt.Println(countsFromPosition)
-	return countsFromPosition[0]
+	for i := len(nums) - 4; i >= 0; i-- {
+		countAtPosition[i] = subCalc(nums, countAtPosition, i)
+	}
+
+	fmt.Println(countAtPosition)
+	return countAtPosition[0]
 }
 
-func subCalc(nums []int, countsFromPosition map[int]int, position int) int {
+func subCalc(nums []int, countAtPosition map[int]int, position int) int {
 	total := 0
 	for i := 0; i < 3; i++ {
 		if nums[position] >= (nums[position+1+i] - 3) {
-			total += countsFromPosition[position+1+i]
+			total += countAtPosition[position+1+i]
 		}
 	}
 
