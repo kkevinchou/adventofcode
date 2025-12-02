f0d23e3c837d5ef5054cef7b449a58bb687eb3df
Kevin C <kkevinchou@hotmail.com>
2025-12-01 22:18:33 -0800

    day2

---
+++ 2025/day2/part1/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var file string = "input.txt"
+
+func main() {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	var total int
+	rangesSplits := strings.Split(string(data), ",")
+	for _, rangeSplit := range rangesSplits {
+		split := strings.Split(rangeSplit, "-")
+		start, _ := strconv.Atoi(split[0])
+		end, _ := strconv.Atoi(split[1])
+
+		for i := start; i <= end; i++ {
+			if isInvalid(i) {
+				total += i
+				fmt.Println(i)
+			}
+		}
+	}
+	fmt.Println(total)
+}
+
+func isInvalid(num int) bool {
+	numAsStr := fmt.Sprintf("%d", num)
+	if len(numAsStr)%2 != 0 {
+		return false
+	}
+
+	firstHalf := numAsStr[:len(numAsStr)/2]
+	secondHalf := numAsStr[len(numAsStr)/2:]
+
+	return firstHalf == secondHalf
+}
+
+// // saved for later
+// // returns the next invalid id, if the input id is already invalid, it returns itself
+// func nextInvalid(input string) (string, int) {
+// 	if len(input)%2 == 0 {
+// 		half := input[:len(input)/2]
+// 		double := half + half
+// 		doubleAsInt, _ := strconv.Atoi(double)
+// 		return double, doubleAsInt
+// 	}
+
+// 	length := len(input)/2 + 1
+// 	half := "1"
+// 	for range length - 1 {
+// 		half += "0"
+// 	}
+// 	double := half + half
+// 	doubleAsInt, _ := strconv.Atoi(double)
+// 	return double, doubleAsInt
+// }
